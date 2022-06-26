85b90cd0c21a322a75640fc75b851e6fbb6e81e3
Ravi <ravi@gmail.com>
2022-06-26 16:25:21 +0530

    ec2 code

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
 ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=FRONTEND -e ROLE=frontend
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------"
 ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=MONGODB -e ROLE=mongodb
 echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
 
