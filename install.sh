c4fd5d5790315bfc37aedb376762641122f56e83
Ravi <ravi@gmail.com>
2022-06-26 16:31:32 +0530

    ec2 code

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 echo -e "\e[31m---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\e[0m\n"
 ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=FRONTEND -e ROLE=frontend
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\e[0m\n"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
-#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=MONGODB -e ROLE=mongodb
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[31m---------------------- DEPLOYING CODE INTO MONGODB------------------------------------\e[0m\n"
+ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=MONGODB -e ROLE=mongodb
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO MONGODB------------------------------------\e[0m\n"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
-#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CATALOGUE -e ROLE=catalogue
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[31m---------------------- DEPLOYING CODE INTO CATALOGUE------------------------------------\e[0m\n"
+ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CATALOGUE -e ROLE=catalogue
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO CATALOGUE------------------------------------\e[0m\n"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
-#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=REDIS -e ROLE=redis
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[31m---------------------- DEPLOYING CODE INTO REDIS ------------------------------------\n"
+ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=REDIS -e ROLE=redis
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO REDIS ------------------------------------\e[0m\n"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
-#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=USER -e ROLE=user
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[31m---------------------- DEPLOYING CODE INTO USER------------------------------------\e[0m\n"
+ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=USER -e ROLE=user
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO USER------------------------------------\e[0m\n"
 
-echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
-#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CART -e ROLE=cart
-echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+echo -e "\e[31m---------------------- DEPLOYING CODE INTO CART------------------------------------\e[0m\n"
+ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CART -e ROLE=cart
+echo -e "\e[32m---------------------- CODE DEPLOYED INTO CART------------------------------------\e[0m\n"
