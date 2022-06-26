2591b31f99d6be02c3593c1543f78d8b988ea373
Ravi <ravi@gmail.com>
2022-06-26 16:24:01 +0530

    ec2 code

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
 ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=FRONTEND -e ROLE=frontend
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
 
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
 ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=MONGODB -e ROLE=mongodb
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
+#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CATALOGUE -e ROLE=catalogue
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
+#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=REDIS -e ROLE=redis
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
+#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=USER -e ROLE=user
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
+
+echo -e "---------------------- DEPLOYING CODE INTO FRONTEND------------------------------------\n"
+#ansible-playbook -i inv.roboshop roboshop.yml -u centos -e ansible_password=DevOps321 -e HOST=CART -e ROLE=cart
+echo -e "---------------------- CODE DEPLOYED INTO FRONTEND------------------------------------\n"
