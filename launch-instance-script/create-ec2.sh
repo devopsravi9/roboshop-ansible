934b5bb84a4a4aaf03ecb87051b17c5185ef98c0
Ravi <ravi@gmail.com>
2022-06-29 14:30:46 +0530

    ec2 code

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -3,6 +3,7 @@
 ##### Change these values ###
 ZONE_ID="Z04748881QTGM14CJWM7A"
 SG_NAME="allow-all-to-public"
+IAM_INSTANCE_PROFILE="Arn=arn:aws:iam::041583668323:instance-profile/role-for-secret-manager-for-roboshop-component"
 #############################
 
 
@@ -14,6 +15,7 @@ create_ec2() {
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
+      --iam-instance-profile="${IAM_INSTANCE_PROFILE}" \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
