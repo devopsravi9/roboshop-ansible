4970fb6eede883d0d1b6cb99663410ebaa9e961d
Ravi <ravi@gmail.com>
2022-11-05 22:48:37 +0530

    ec2 code

---
+++ task/roles/create-s3/templates/terraform.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "{{AWS_ACCESS_KEY}}"
+  secret_key = "{{AWS_SECRET_KEY}}"
+}
+
+resource "aws_s3_bucket" "task" {
+  bucket = "task-970141"
+  tags = {
+    Name = "task"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.task.id
+  acl    = "private"
+}
+
+//resource "null_resource" "copy" {
+//    provisioner "local-exec" {
+//        command = "aws s3 cp main.tf s3://task-970141/task"
+//    }
+//}
+//
+//resource "aws_s3_object" "object" {
+//  bucket = "task-970141"
+//  key    = "task/main.tf"
+//  source = "/home/centos/roboshop-ansible/task/main.tf"
+//  etag = filemd5("/home/centos/roboshop-ansible/task/main.tf")
+//}
