dafc6d65165e69a37ac8f8a3e7cd04fcfdf4c951
Ravi <ravi@gmail.com>
2022-11-06 17:19:27 +0530

    ec2

--- task/s3/main.tf
+++ task/s3/main.tf
@@ -14,6 +14,15 @@ resource "aws_s3_bucket_acl" "private_bucket" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 
 
 //resource "null_resource" "copy" {
